cdbf5195381b49270ba02a99e1efb5852f84e74a
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-01-24 13:05:32 +0530

    js

---
+++ javascript/first.js
@@ -0,0 +1,26 @@
+let name = "somnath";
+let age = 12;
+let city = "kathar";
+
+
+// Redeclaration
+// var (Allowed)
+// var a = 5;
+// var a = 10;
+// console.log(a); // 10
+
+// let (NOT Allowed)
+// let b = 5;
+// let b = 10; // ❌ Error
+
+
+let age1 = "12"
+
+
+
+
+console.log(name);
+console.log(age);
+console.log(city);
+console.log(typeof city);
+console.log(Number(age1));
---
+++ javascript/index.js
@@ -0,0 +1,57 @@
+// let a = 25;
+// let name = "aryan";
+// let isStudent = true;
+// let x;
+// let y = null;
+
+// console.log(a);
+// console.log(name);
+// console.log(isStudent);
+// console.log(y);
+
+
+// // typeof
+// console.log(typeof a);
+// console.log(typeof name);
+// console.log(typeof isStudent);
+// console.log(typeof y);
+
+// // ARITHMATIC OPERATION
+
+// let num1 = 6;
+// let num2 = 10
+// let char = "xyz"
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+// console.log(num1 * num2);
+// console.log(char / num1);
+
+
+// // number conversion
+
+// let numb = "10";
+// console.log(Number(numb));
+// console.log(parseInt("20"));
+// console.log(parseFloat("3.5"));
+
+// // string concatenation
+// let s1 = "a";
+// let s2 = "b";
+// let s3 = "c";
+
+// console.log(s1+ " " + s2 + " " + s3);
+
+// string length
+
+let message = "hello";
+console.log(message.length);
+console.log(message.toLowerCase);
+console.log(message.toUpperCase);
+
+
+
+
+
