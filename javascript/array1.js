5403786091203367f85e80ff3cc25fcd6c99f54f
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-01-28 14:27:40 +0530

    array

---
+++ javascript/array1.js
@@ -0,0 +1,28 @@
+// array
+
+// let arr = [23,32,4,54,63,46,35,"hello","lpu"];
+// console.log(arr[7]);
+// console.log(arr.length);
+
+
+let arr1 = [23,32,4,54,63,46,35,34,76,87,65,47,44];
+
+// arr1.push(60);
+// arr1.pop(4);
+// arr1.pop();
+
+// arr1[2]=100;
+// console.log(arr1);
+
+// question create an array of 5 students name
+// print first name
+// add new name
+// change one name
+
+
+arr3 = ["aryan","som","asis","ritik"]
+console.log(arr3[0]);
+arr3.push("singh");
+console.log(arr3);
+arr3[2] = "ashish";
+console.log(arr3);
