115945cf6279cd5ba70746754a75e58c25bdfd4a
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-01-28 14:31:49 +0530

    object

---
+++ javascript/object.js
@@ -0,0 +1,15 @@
+let obj = {
+    name:"Aryan",
+    city:"Jalandhar",
+    age:24
+
+}
+
+console.log(obj);
+
+console.log(obj.name);
+console.log(obj.city);
+console.log(obj.age);
+
+obj.class = "B.tech";   //to add new properties
+console.log(obj);
