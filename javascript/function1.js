1c7398014b16207926073dc7618aa75a38e71662
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-01-28 14:13:03 +0530

    function

--- javascript/function1.js
+++ javascript/function1.js
@@ -3,3 +3,91 @@
 // function is a reusable block of Code 
 // avoid repetation
 // makes code clean and organised
+
+// function greet(){
+//     console.log("Hello");
+// }
+
+// function greet(a){
+//     console.log("Hello");
+// }
+
+// greet(5);
+
+
+// function add(a,b){
+//     console.log(a+b)
+        
+// }
+// add(false,true);
+// add(34,54);
+// add(34.5,34.34);
+
+
+// function multi(a,b){
+//     console.log(a*b);
+// }
+
+// console.log(multi(234,32));
+
+
+// function add(c,d){
+//     return(c+d);
+// }
+
+// let result = add(23,423);
+// console.log(add(23,423));
+
+
+// function supports hosting (temporal deadzone)
+
+// xyz(); //reference error
+
+// let xyz = function test(){
+//     console.log("hello my name is aryan");
+// }
+
+//anonymous function
+
+// let showMsg = function () {
+//     console.log("I am anonymous function");
+// }
+
+// showMsg();
+
+
+
+// arrow function 
+
+// let functionName = () =>{    //syntax
+// }
+
+//
+// let hello = () =>{
+//     console.log("my name is aryan");
+// }
+
+// hello();
+
+//
+// let arrow1 = (a,b) =>{
+//     console.log(a+b);
+// }
+
+// arrow1(23,42);
+
+//
+// let square1 = (x) => x * x;
+// console.log(square1(4));
+
+
+
+//IIFE = function that runs immidiately after creating 
+// or it runs automatically
+
+// (function(name,surname1){
+//     console.log("Hello "+name);
+//     console.log(`Hello my name is ${name} ${surname1}.`);
+// })("Aryan","Singh");
+
+
