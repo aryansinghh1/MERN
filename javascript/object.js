488c53c3e024d8be09781ec054b7fc6160bd2e6f
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-01-28 14:53:14 +0530

    object

--- javascript/object.js
+++ javascript/object.js
@@ -1,15 +1,60 @@
-let obj = {
-    name:"Aryan",
-    city:"Jalandhar",
-    age:24
+// let obj = {
+//     name:"Aryan",
+//     city:"Jalandhar",
+//     age:24
 
-}
+// }
 
-console.log(obj);
+// console.log(obj);
 
-console.log(obj.name);
-console.log(obj.city);
-console.log(obj.age);
+// console.log(obj.name);
+// console.log(obj.city);
+// console.log(obj.age);
 
-obj.class = "B.tech";   //to add new properties
-console.log(obj);
+// obj.class = "B.tech";   //to add new properties
+// console.log(obj);
+
+
+// delete obj.name;        // to remove any properties
+// console.log(obj);
+
+
+//array of object
+
+// let user = [
+//     {name: "ar", age:20},
+//     {name: "sdr", age:50},
+//     {name: "arsf", age:40},
+//     {name: "arbfd", age:2},
+
+// ]
+
+// console.log(user[3]);
+// console.log(user);
+
+// console.log(user[2].name);
+
+
+// let array1 = ["apple","mango","orange","watermelon"];
+// for (let i =0;i<array1.length;i++){
+//     console.log(array1[i]);
+// }
+
+
+
+// array with for each loop 
+
+
+// let obj = {
+//     name:"Aryan",
+//     city:"Jalandhar",
+//     age:24
+
+// }
+
+// for(let key in obj){
+//     console.log(key,obj[key]);
+//     console.log(obj[key]);
+// }
+
+
