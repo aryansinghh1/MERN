2ee0b2fe417f3aabba1e39feb6ebb455167bbf90
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-01-29 15:33:14 +0530

    deligation

--- javascript/index2.js
+++ javascript/index2.js
@@ -16,18 +16,37 @@
 //     heading.innerText = "button Clicked!"
 // ));
 
-document.getElementById("btn").addEventListener("click",()=>{
-let el = document.getElementById("title");
-el.innerText="change to see get element by id";
-el.style.color = "red";
-el.style.background = "yellow";
-})
+// document.getElementById("btn").addEventListener("click",()=>{
+// let el = document.getElementById("title");
+// el.innerText="change to see get element by id";
+// el.style.color = "red";
+// el.style.background = "yellow";
+// })
 
-document.getElementById("btn1").addEventListener("click",() =>{
-let gclass = document.getElementsByClassName("para");
-for (let i = 0 ; i< gclass.length; i++){
-    gclass[i].classList.toggle("highlight");
-    gclass[i].style.color = "red";
-    gclass[i].style.border = "2px solid blue";
-}
-});
+// document.getElementById("btn1").addEventListener("click",() =>{
+// let gclass = document.getElementsByClassName("para");
+// for (let i = 0 ; i< gclass.length; i++){
+//     gclass[i].classList.toggle("highlight");
+//     gclass[i].style.color = "red";
+//     gclass[i].style.border = "2px solid blue";
+// }
+// });
+
+// document.getElementById("testbtn").addEventListener("click",(e)=>{
+//     e.target.style.backgroundColor="red";
+//     e.target.style.color="blue";
+// });
+
+
+
+//callback function
+function greet(name){
+    console.log("hello "+ name);
+};
+
+function processUser(callback){
+let name = "ankit";
+callback(name);
+};
+
+processUser(greet);
