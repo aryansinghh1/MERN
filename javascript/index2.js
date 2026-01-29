dfe0ed0c417068ef22d6d0625449f1603be5f291
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-01-29 09:10:01 +0530

    eventlistentner

--- javascript/index2.js
+++ javascript/index2.js
@@ -1,2 +1,17 @@
+let heading = document.getElementById("title");
+heading.innerText = "hello from javascript";
+heading.style.color ="blue";
 
-console.log("Hello, the script is running!");
+let paragraph = document.getElementsByClassName("para");
+paragraph[0].innerText = "welcome home";
+paragraph[1].innerText = "this is java script para";
+
+paragraph[0].style.color = "red";
+paragraph[1].style.color = "blue";
+
+
+let button1 = document.getElementById("btn");
+button1.addEventListener("click",() =>(
+    // button1.innerText = "button Clicked"
+    heading.innerText = "button Clicked!"
+));
