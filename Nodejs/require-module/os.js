8c893844ca725f172f9e006b84134d2e04ff73b9
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-02 16:42:43 +0530

    cpu information

--- Nodejs/require-module/os.js
+++ Nodejs/require-module/os.js
@@ -3,3 +3,9 @@ import os from "os";  //os is built in module of js used to retrive system infor
 console.log("OS type: ",os.type());
 console.log("OS platform: ",os.platform());
 console.log("OS architecture: ",os.arch());
+
+//cpu information
+const cpus = os.cpus();
+
+console.log("CPU CORES: ", cpus.length);
+console.log("CPU model: ", cpus[1].model);
