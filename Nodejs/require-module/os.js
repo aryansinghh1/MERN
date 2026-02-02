c66661b7c0d3015c4b0f06bd426ffc555cac88b0
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-02 16:38:15 +0530

    operating system

---
+++ Nodejs/require-module/os.js
@@ -0,0 +1,5 @@
+import os from "os";  //os is built in module of js used to retrive system information
+
+console.log("OS type: ",os.type());
+console.log("OS platform: ",os.platform());
+console.log("OS architecture: ",os.arch());
