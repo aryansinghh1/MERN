7577a7ef266a54a1baede2161c2ac43720059f6b
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-03 14:32:44 +0530

    pushing html page with http

--- Nodejs/http/creating-live-server.js
+++ Nodejs/http/creating-live-server.js
@@ -16,22 +16,48 @@ import http from "http";
 
  
 //
-const server = http.createServer((req, res)=>{
-    const responseObj = {
-        success: true,
-        message: "User created",
-        name:"aryan",
-        age: 20,
-    };
+// const server = http.createServer((req, res)=>{
+//     const responseObj = {
+//         success: true,
+//         message: "User created",
+//         name:"aryan",
+//         age: 20,
+//     };
 
-    res.writeHead(200, {
-        "Content-Type": "application/json",
-    });
-    res.end(
-        JSON.stringify(responseObj)
-    );
+//     res.writeHead(200, {
+//         "Content-Type": "application/json",
+//     });
+//     res.end(
+//         JSON.stringify(responseObj)
+//     );
+// });
+
+// server.listen(3000, ()=>{
+//     console.log("🚀 Server running at http://localhost:3000");
+// });
+
+
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+  });
+
+  res.end(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Node Server</title>
+      </head>
+      <body>
+        <h1>User Created</h1>
+        <p>Name: Aryan</p>
+        <p>Age: 20</p>
+      </body>
+    </html>
+  `);
 });
 
-server.listen(3000, ()=>{
-    console.log("🚀 Server running at http://localhost:3000");
+server.listen(3000, () => {
+  console.log("🚀 Server running at http://localhost:3000");
 });
