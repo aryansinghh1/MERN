f0745f216b8fa4dc3f85ee30afb702840a6f11f2
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-03 14:22:38 +0530

    live server

--- Nodejs/http/creating-live-server.js
+++ Nodejs/http/creating-live-server.js
@@ -27,7 +27,9 @@ const server = http.createServer((req, res)=>{
     res.writeHead(200, {
         "Content-Type": "application/json",
     });
-    res.end(JSON.stringify(responseObj));
+    res.end(
+        JSON.stringify(responseObj)
+    );
 });
 
 server.listen(3000, ()=>{
