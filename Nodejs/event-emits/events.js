4c55f31346d9f970004679adbb68aad4a3aa9bdb
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-03 09:43:20 +0530

    events

--- Nodejs/event-emits/events.js
+++ Nodejs/event-emits/events.js
@@ -1,1 +1,23 @@
+//importing eventemitter
 import EventEmitter from "events";
+
+
+//create a event Emitter
+const emitter = new EventEmitter();
+
+
+//register an event listener
+// can be fire more than once
+emitter.on("userRegistered", (username) => {
+  console.log("User registered", username);
+});
+
+
+//can be fire once
+// emitter.once("userRegistered",(username)=>{
+//     console.log("User registered",username);
+// });
+
+//emit the event
+emitter.emit("userRegistered","Aryan");
+emitter.emit("userRegistered","Aryan");
