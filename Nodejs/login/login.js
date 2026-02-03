36ef34289273dd7ff69756df0d821e61e37ff438
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-03 11:11:25 +0530

    login

--- Nodejs/login/login.js
+++ Nodejs/login/login.js
@@ -1,4 +1,33 @@
+import { error } from "console";
+import { sign } from "crypto";
 import EventEmitter from "events";
 
 const emitter = new EventEmitter();
 
+emitter.on("loginSuccess",()=>{
+    console.log("Login Successfull");
+})
+
+emitter.on("error",(err)=>{
+    console.log("Error ❌ ",err.message);  
+})
+
+function login(isLoggedIn){
+    console.log("Checking login status..");
+
+    try{
+        if(isLoggedIn === true){
+
+            emitter.emit("loginSuccess")
+        }
+        else{
+            throw new Error("Login Failed");
+        }
+
+    }catch (err){
+
+        emitter.emit("error",err);
+    }
+}
+
+login(false);
