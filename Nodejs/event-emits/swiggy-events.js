b0f7f046e3b2d61f288d0b1b7f6c9bcb14f5ef21
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-03 09:54:12 +0530

    multiple events fire

---
+++ Nodejs/event-emits/swiggy-events.js
@@ -0,0 +1,25 @@
+import EventEmitter from "events";
+
+const emmiter = new EventEmitter();
+
+emmiter.on("trig",()=>{
+    console.log("Your order is placed");
+})
+
+emmiter.on("trig",()=>{
+    console.log("Delivery partner will assigned shortly!!");
+})
+
+emmiter.on("trig",()=>{
+    console.log("Your order is preparing");
+})
+
+emmiter.on("trig",()=>{
+    console.log("Your order is out for delivery");
+})
+
+emmiter.on("trig",()=>{
+    console.log("Your order is delivered");
+})
+
+emmiter.emit("trig");
