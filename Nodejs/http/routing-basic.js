f0091c5d679e2bb037ec3b80807c8d9fb0a20bbd
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-04 09:32:38 +0530

    url routing

---
+++ Nodejs/http/routing-basic.js
@@ -0,0 +1,22 @@
+import http from "http";
+
+const server = http.createServer((req, res)=>{
+    if(req.url === "/"){
+        res.end("Home Page");
+    }
+    else if(req.url === "/about"){
+        res.end("About page");
+    }
+    else if(req.url === "/contact"){
+        res.end("Contact Page");
+    }
+    else{
+        res.statusCode = 404;
+        res.end("Page not found");
+    }
+    
+})
+
+server.listen(3000, ()=>{
+        console.log("Server running on http://localhost:3000");
+    })
