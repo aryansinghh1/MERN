c13e11445a74679f95c45324178180014d5905ed
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-16 12:03:27 +0530

    lecture6

---
+++ React/lecture6.1/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ContactForm from "../src/components/ContactForm";
+
+export default function App(){
+    return(
+        <div style ={{padding: "40px"}}>
+        <h1>Contact Me</h1>
+        <ContactForm/>
+        </div>
+    );
+}
