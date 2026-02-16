ede145accbf9be2739c8d15f122dac5d53e1085c
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-16 15:42:58 +0530

    making form using useState

--- React/lecture8(counter)/src/components/ContactForm.jsx
+++ React/lecture8(counter)/src/components/ContactForm.jsx
@@ -1,23 +1,40 @@
 import React, { useState } from "react";
 
+// Requirements
+// 1️⃣ Create a Contact Form with fields:
+// Name input
+// Email input
+// Message textarea
+// Submit button
+
+// 2️⃣ Use React Events
+// You must use:
+// onChange
+// onClick
+// onKeyUp or onKeyDown
+
+// Expected behavior:
+// Typing → should update state
+// Key press → should console log message
+// Button click → should show alert with entered data
 export default function ContactForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [submittedName, setSubmittedName] = useState("");
-
-
+  const [message, setMessage] = useState("");
+  // const [submittedName, setSubmittedName] = useState("");
 
   function handleSubmit(e) {
-    e.preventDefault();      // stops page refresh
+    e.preventDefault();    //stops page refresh
     const name = e.target.elements[0].value;
     const email = e.target.elements[1].value;
-
-    setSubmittedName(name);
-    alert(`Uncontrolled Submitted ${name} ${email}`);
+    const message = e.target.elements[2].value;
+    // setSubmittedName(name);
+    alert(`Name: ${name}\nEmail ${email}\nMessage: ${message}`);
   }
 
   return (
     <form onSubmit={handleSubmit} style={{ textAlign: "center" }}>
+      <br /><br />
       <input
         type="text"
         placeholder="Enter name"
@@ -39,11 +56,19 @@ export default function ContactForm() {
       />
       <br />
       <br />
+      <textarea
+        placeholder="Enter message"
+        onChange={(e) => setMessage(e.target.value)}
+        onKeyUp={() => console.log("key released")}
+        onKeyDown={() => console.log("key pressed")}
+      ></textarea>
+      <br />
+      <br />
       <button type="submit" onMouseUp={() => console.log("mouse up")}>
         Submit
       </button>
       <p>Live Name (controlled): {name}</p>
-      <p>Submitted Name (Uncontrolled): {submittedName}</p>
+      {/* <p>Submitted Name (Uncontrolled): {submittedName}</p> */}
     </form>
   );
 }
